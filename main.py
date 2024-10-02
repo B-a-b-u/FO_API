173bc7e2c9ede376151e034b050157808effd4ba
Baburaj <sbrbabu123@gmail.com>
2024-10-02 23:24:31 +0530

    Fixed bug in pdf table extraction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body, HTTPException, UploadFile, File, Query
+from fastapi import FastAPI, HTTPException, UploadFile, File, Query
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
@@ -10,19 +10,17 @@ import io
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 
-# Load the model
 model = tf.keras.models.load_model('models/model2-90.h5')  
 
-# Initialize FastAPI app
+
 app = FastAPI()
 
-# Allow CORS 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],  
+    allow_headers=["*"], 
 )
 
 soil_map = {0: 'Black', 1: 'Clayey', 2: 'Loamy', 3: 'Red', 4: 'Sandy'}
@@ -33,51 +31,44 @@ fertilizer_map = {0: '10-26-26', 1: '14-35-14', 2: '17-17-17', 3: '20-20', 4: '2
 
 def extract_soil_data(pdf_content):
     pdf_file = io.BytesIO(pdf_content)
-    with pdfplumber.open(pdf_file) as pdf:
-        # Assuming the table is on the first page
-        page = pdf.pages[0]
-        table = page.extract_table()
-        
-        # Extract headers and rows
-        headers = table[0]
-        rows = table[1:2]
-        
-        # Find the index of the PH Value column
-        ph_index = headers.index('PH value')
-        # soil_type = rows[0][0].strip()
-        # nitrogen = rows[0][-3][:-3]
-        # phosphorous = rows[0][-2][:-3]
-        # potassium = rows[0][-1][:-3]
+    table = None
+    with pdfplumber.open(pdf_file) as file:
+        table = file.pages[0].extract_tables()[0]
+        print(f"table : {table} tables : {len(table)} tablel[] : {table[0][:]}")
+    headers = table[0][:]
+    print(f"headers : {headers}")
+    
+    soil_type_index = headers.index('Soil Type')
+    n_index,p_index,k_index = 0,0,0
+    for i,h in enumerate(headers):
+        if h.lower().startswith("nitrogen") or h.lower() == "n":
+            n_index = i
+        elif h.lower().startswith("phosphorus") or h.lower() == "p":
+            p_index = i
+        elif h.lower().startswith("potassium") or  h.lower() =="k":
+            k_index = i
+    soil_data = table[1][:]
+    print(f"soil data:{soil_data} n index: {n_index} p index : {p_index} k index : {k_index}")
 
-        soil_type = rows[0][0].strip()
-        nitrogen = float(rows[0][-3])
-        phosphorous = float(rows[0][-2])
-        potassium = float(rows[0][-1])
-        print(rows)
-        print(f"N : {nitrogen} P:{phosphorous} k:{potassium}")
-
-        return {
-            "soil_type":soil_type,
-            "nitrogen" : nitrogen,
-            "phosphorous":phosphorous,
-            "potassium":potassium,
-        }
-
-# Replace 'SoilType.pdf' with the path to your PDF file
-# print(extract_soil_data_excluding_ph("report/Soil Type_1_33.pdf"))
+    soil_type = soil_data[soil_type_index].strip()
+    nitrogen = float(soil_data[n_index]) 
+    phosphorous = float(soil_data[p_index]) 
+    potassium = float(soil_data[k_index]) 
+    print(f"ty : {soil_type} n : {nitrogen} p :{phosphorous} k : {potassium}")
+    return {
+        "soil_type": soil_type,
+        "nitrogen": nitrogen,
+        "phosphorous": phosphorous,
+        "potassium": potassium,
+    }
 
 def get_weather(location):
     api_url = f"http://api.weatherapi.com/v1/forecast.json?key=6bac00c67d4144e5ad2180607240809&q={location[0]},{location[1]}&days=1&aqi=no&alerts=no"
-    print(api_url)  # Print the URL to debug
+    print(api_url) 
     response = requests.get(api_url)
-    
-    # Check for errors in the response
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail="Error fetching weather data")
-    
-    # Return the weather data as JSON
     weather_data = response.json()
-
     current_weather = weather_data.get('current', {})
     temperature = current_weather.get('temp_c', None)
     humidity = current_weather.get('humidity', None)
@@ -89,9 +80,6 @@ def get_weather(location):
     }
     print(data)
     return data
-
-# print(get_weather([10.995671, 76.932291]))
-
 
 def preprocess_data(data):
     soil_encoder = LabelEncoder()
@@ -119,15 +107,15 @@ async def predict_fertilizer(file: UploadFile = File(...),
                             lon: float = Query(..., description="Longitude of the location"),
                             crop_type: str = Query(..., description="Type of the crop")
     ):
+    print(f"lattitude : {lat} \nlongitude : {lon} \ncroptype : {crop_type}")
     pdf_content = await file.read()
-    print(f"Read file : {pdf_content}")
+    # print(f"Read file : {pdf_content}")
     soil_data = extract_soil_data(pdf_content)
     soil_type = soil_data["soil_type"]
     n = soil_data["nitrogen"]
     p = soil_data["phosphorous"]
     k = soil_data["potassium"]
-    
-    # Get weather data
+
     weather_data = get_weather([lat, lon])
     t = weather_data['Temperature']
     h = weather_data['Humidity']
@@ -145,8 +133,7 @@ async def predict_fertilizer(file: UploadFile = File(...),
     }
     print(f"data : {data}")
     df = pandas.DataFrame(data)
-    print(f"df : {df}")  # Print the DataFrame to ch eck
-
+    print(f"df : {df}") 
 
     processed_data = preprocess_data(df)
     print(f"process data : {processed_data}")
